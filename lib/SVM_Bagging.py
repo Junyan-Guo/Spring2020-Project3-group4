fdeaccf58c6f865d07788f8839ac00ca47e1fb9c
Raymond-601 <60190417+Raymond-601@users.noreply.github.com>
2020-05-28 20:40:47 -0400

    Update SVM_Bagging.py

--- lib/SVM_Bagging.py
+++ lib/SVM_Bagging.py
@@ -10,10 +10,12 @@ import numpy as np
 import os
 import pandas as pd
 import time
-from scipy.spatial.distance import cdist
-from sklearn.svm import SVC
 from scipy.io import loadmat
-# from sklearn.model_selection import train_test_split, cross_validate
+from scipy.spatial.distance import cdist
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 from sklearn.ensemble import BaggingClassifier
 
 
@@ -26,7 +28,6 @@ from sklearn.ensemble import BaggingClassifier
         2. For Test_set:
             1) Should have a file named points that contains the .mat file contains image point coordinates
 """
-
 
 def extract_mat(x):
     v = list(x.keys())[-1]
@@ -43,11 +44,24 @@ def get_f(file_dir):
     r = b[np.triu_indices(b.shape[1], 1)].flatten()
     return r
 
+def f_pca(x):
+    my_pca = PCA(n_components = 130)
+    new_X = my_pca.fit_transform(x)
+    compo = sum(my_pca.explained_variance_ratio_)
+    print(f'The Decomposition take up {compo: 0.2f}% Information of original Data')
+    
+    return new_X, my_pca
+
 def feature_extraction(dir_x):
+    if (dir_x[-1] != '/'):
+        dir_x = dir_x + '/'
+    
     fea_start = time.time()
-    filenames = list(os.listdir(dir_x + '/'))
+    
+    filenames = list(os.listdir(dir_x))
     filenames.sort()
-    X = np.array(list(map(get_f, ((dir_x + '/' + i) for i in filenames))))
+    X = np.array(list(map(get_f, ((dir_x + i) for i in filenames))))
+    
     fea_end = time.time()
     fea_time = fea_end - fea_start
     
@@ -55,9 +69,26 @@ def feature_extraction(dir_x):
     print(f'Feature Extraction Cost: {fea_time: 0.2f} Seconds')
     return X
 
-def BaggingSVM(X_path, y_path):
-    X = feature_extraction(X_path)
-    y = pd.read_csv(y_path).emotion_idx
+def BaggingSVM(train_X, train_y):
+    start_SVM = time.time()
+    S_svm = SVC(C = 0.1,
+                kernel = 'linear',
+                shrinking = True,
+                decision_function_shape = 'ovo')
+    Bagg_SVM = BaggingClassifier(S_svm,
+                                 n_estimators = 80,
+                                 n_jobs = 5,
+                                 bootstrap_features = True)
+    Bagg_SVM.fit(train_X, train_y)
+    end_SVM = time.time()
+    
+    Train_time = end_SVM - start_SVM
+    print(f'The Time for train is: {Train_time: 0.2f} Seconds')
+    return Bagg_SVM
+
+
+def BaggingSVM_w_pca(train_X, train_y):
+    train_X, pca_mode = f_pca(train_X)    
     
     start_SVM = time.time()
     S_svm = SVC(C = 0.1,
@@ -68,13 +99,28 @@ def BaggingSVM(X_path, y_path):
                                  n_estimators = 80,
                                  n_jobs = 5,
                                  bootstrap_features = True)
-    Bagg_SVM.fit(X,y)
+    Bagg_SVM.fit(train_X, train_y)
     end_SVM = time.time()
     
     Train_time = end_SVM - start_SVM
     print(f'The Time for train is: {Train_time: 0.2f} Seconds')
-    return Bagg_SVM
+    return Bagg_SVM, pca_mode
 
+
+def claim_possible_test_error(X_path, y_path, n_iter = 1):
+    X = feature_extraction(X_path)
+    y = pd.read_csv(y_path).emotion_idx
+    
+    accs = []
+    for i in range(n_iter):
+        trainx, testx, trainy, testy = train_test_split(X, y, test_size = .2)
+        model, pca_mode= BaggingSVM_w_pca(trainx, trainy)
+        new_testx = pca_mode.transform(testx)
+        testy_hat = model.predict(new_testx)
+        accs.append(accuracy_score(testy, testy_hat))
+    return np.mean(accs)
+
+claim_possible_test_error('../data/points', '../data/label.csv', 5)
 
 def Test_on(test_model, Test_set_dir):
     X = feature_extraction(Test_set_dir)
@@ -82,8 +128,20 @@ def Test_on(test_model, Test_set_dir):
     return y_hat
 
 
+X = feature_extraction('../data/points')
+y = pd.read_csv('../data/label.csv').emotion_idx
+my_pca = PCA(n_components = 130)
+new_X = my_pca.fit_transform(X)
+a = my_pca.explained_variance_ratio_
+    
+
 TheModel = BaggingSVM('../data/points', '../data/label.csv')
 y_hat = Test_on(TheModel, '../data/points')
+
+
+
+a,m = f_pca(X)
+m.fit_transform(X)
 
 
 # =============================================================================
